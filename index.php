7c041d86cdad07241c5e28d45c8bf034e1c33591
kamatris <maksystem10@gmail.com>
2017-03-18 19:09:57 +0100

    Second commit

--- index.php
+++ index.php
@@ -5,21 +5,135 @@
     <title>OtherCars</title>
     <link rel="stylesheet" href="web/css/bootstrap.min.css">
     <link rel="stylesheet" href="web/css/bootstrap-theme.min.css">
-    <link rel="stylesheet" href="web/css/style.css">
+    <link rel="stylesheet" href="web/css/stylefront.css">
 </head>
 <body>
-<div class="container-fluid">
-    <div class="row">
-        <header class="col-md-12">Header Block</header>
-    </div>
-    <div class="row">
-        <section class="col-md-12">Section Content</section>
-    </div>
-    <div class="row">
-        <footer class="col-md-12">Footer Page</footer>
-    </div>
-</div>
+<div class="container">
+   <div class="row">
+       <header class="col-md-12">
+           <div class="col-md-3">
+               <img src="web/images/logo.png" alt="" class="logo">
+           </div>
+           <div class="col-md-6">
+               <div class="description">
+                   <h2>Other Cars</h2>
+                   <h4>
+                       Le meilleur moyen pour vos vacances!!!
+                   </h4>
+               </div>
+           </div>
+           <div class="col-md-3 imgheader">
+               <img src="web/images/img_header1.jpg">
+           </div>
+       </header>
+   </div>
+   <div class="row">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Accueil</a></li>
+            <li><a href="#">Nos voitures</a></li>
+            <li><a href="#">Reservation</a></li>
+            <li><a href="#">Nos Tarifs</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Contact </a></li>
+          </ul>
+        </div>
+   </div>
+   <div class="row">
+       <div class="col-md-4">
+           <fieldset>
+               <legend>Reservation</legend>
+               <form class="form-group">
+                    <div class="col-md-12">
+                        <label>Vehicule </label>
+                        <select name="" class="form-control">
+                            <option>Sample 1</option>
+                            <option>Sample 1</option>
+                            <option>Sample 1</option>
+                        </select>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label>Lieu de Prise </label>
+                            <select name="" class="form-control">
+                                <option>Sample 1</option>
+                                <option>Sample 1</option>
+                                <option>Sample 1</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label>Lieu de Remise </label>
+                            <select name="" class="form-control">
+                                <option>Sample 1</option>
+                                <option>Sample 1</option>
+                                <option>Sample 1</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label>Date de Prise </label>
+                            <input type="date" class="form-control">
+                            <label>Heure de Prise </label>
+                            <input type="time" class="form-control">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label>Date de Remise </label>
+                            <input type="date" class="form-control">
+                            <label>Heure de Remise </label>
+                            <input type="time" class="form-control">
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                       <div class="form-group">
+                            <button type="submit" class="submit btn btn-info"><span class="glyphicon glyphicon-ok-circle"></span> Reserver</button>
+                            <button type="reset" class="btn btn-danger"><span class="glyphicon glyphicon-remove-circle"></span> Annuler</button>
+                       </div>
+                    </div>
+               </form>
+           </fieldset>
+       </div>
+       <div class="col-md-8"></div>
+   </div>
+   <div class="row">
+       <div class="col-md-12">
+           <div class="col-md-12">
+              <h3> Other Cars :</h3>
+              <p>Le leader de la location de voiture en tunisie, avec notre societe louez en toute securité avec les Prix les moins chers.</p>  
+           </div>
+           <div class="col-md-3 col-md-offset-2">
 
+               <ul>
+                   <li><b>Tel :</b></li>
+                    <li>00216 98 958 377</li>
+                    <li>00216 24 072 434</li>
+                    <li>00216 24 074 112</li>
+                    <li>00974 666 16 519</li>
+                    <li>0091 55 834 6758</li>
+               </ul>
+           </div>
+           <div class="col-md-3">
+               <ul>
+                   <li><b>E-mail :</b></li>
+                   <li>other.cars2016@gmail.com</li>
+                   <li>tarekabidi@ymail.com</li>
+                   <li>otrabelsi@hmc.org.qa </li>
+               </ul>
+           </div>
+       </div>
+   </div>
+</div>
 <script src="web/lib/jquery.min.js"></script>
 <script src="web/lib/bootstrap.min.js"></script>
 </body>
